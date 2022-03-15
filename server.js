bc4e022c3ca7540d9f67bfa1c4b04465b7abca4c
Aldo Quintero <aldoq95@gmail.com>
2022-03-14 18:14:28 -0700

    mongon db setup added

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+connectDB();
+
 app.get('/', (req, res) => res.json({ msg: 'Welcome to FutHub' }));
+
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
 
 const PORT = process.env.PORT || 4000;
 
