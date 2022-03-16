f57f38c503e7bfcf23751e190823099b11c2ade5
Aldo Quintero <aldoq95@gmail.com>
2022-03-16 15:06:56 -0700

    mongoose added

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const connectDB = require('./config/db');
-
+const { path } = require('express/lib/application');
+const connectDB = require('./backend/database/db');
+require('dotenv').config();
 const app = express();
 
 connectDB();
 
 app.get('/', (req, res) => res.json({ msg: 'Welcome to FutHub' }));
 
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
+app.use('/api/users', require('./backend/routes/users'));
+app.use('/api/auth', require('./backend/routes/auth'));
 
 const PORT = process.env.PORT || 4000;
 
